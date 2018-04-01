922418f97b166553d3453b8eb60aab80daaeb2c8
yugeshK <yugesh.kothari@gmail.com>
2018-04-01 07:39:47 +0530

    base url

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='http://worldnewsense.herokuapp.com/theme.html'), name='themed-webpage'),
 	url(r'getEvents', views.getEvents, name='getEvents'),
 	url(r'pageData',views.pageData,name='pageData')
 ]
